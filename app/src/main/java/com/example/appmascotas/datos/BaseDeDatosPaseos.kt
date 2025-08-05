fed7919358282e0982ffb7f17bcac3f6eb3b3d8d
DarkKapo <darkkapo@gmail.com>
2025-08-05 13:06:53 -0400

    agrego la logica a BaseDeDatosPaseos

--- app/src/main/java/com/example/appmascotas/datos/BaseDeDatosPaseos.kt
+++ app/src/main/java/com/example/appmascotas/datos/BaseDeDatosPaseos.kt
@@ -1,4 +1,34 @@
 package com.example.appmascotas.datos
 
-class BaseDeDatosPaseos {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [EntidadPaseoMascota::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(ConvertidoresDeTipo::class)
+abstract class BaseDeDatosPaseos : RoomDatabase() {
+    abstract fun AccesoDatosPaseos() : AccesoDatosPaseos
+
+    companion object {
+        @Volatile
+        private var INSTANCIA: BaseDeDatosPaseos? = null
+
+        fun obtenerBD(context: Context): BaseDeDatosPaseos {
+            return INSTANCIA ?: synchronized(this) {
+                val instancia = Room.databaseBuilder(
+                    context.applicationContext,
+                    BaseDeDatosPaseos::class.java,
+                    "base_datos_paseos"
+                ).build()
+                INSTANCIA = instancia
+                instancia
+            }
+        }
+    }
 }
